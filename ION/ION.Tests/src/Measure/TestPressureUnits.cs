3d4dbe229e721b4661d4836c0752bc3cc751ab0b
Andrew Hodder <ahodder@appioninc.com>
2015-10-27 10:54:26 -0600

    Fixed some tests. Added packages to ion droid.

--- ION/ION.Tests/src/Measure/TestPressureUnits.cs
+++ ION/ION.Tests/src/Measure/TestPressureUnits.cs
@@ -167,9 +167,6 @@ namespace ION.TestFixtures.Measure {
     public void TestPSIAToKPA() {
       AssertEquals(27.579, Units.Pressure.PSIA.OfScalar(4).ConvertTo(Units.Pressure.KILOPASCAL).amount);
     }
-
-    public static void Main() {
-    }
   }
 }
 
