2d2e2dd9eab8300ba1de1e0232d2ffb2de7c0896
Andrew Hodder <ahodder@appioninc.com>
2015-11-04 09:51:43 -0700

    Removed .DS_STORE from project. Again. Also commited some android things.

---
+++ ION/ION.Droid/src/Widgets/Adapters/Navigation/NavigationCategory.cs
@@ -0,0 +1,22 @@
+﻿namespace ION.Droid.Widgets.Adapters.Navigation {
+  
+  using System;
+
+  /// <summary>
+  /// A simple container for navigation items.
+  /// </summary>
+  public class NavigationCategory {
+
+    /// <summary>
+    /// The title of the category which all its items fall under.
+    /// </summary>
+    /// <value>The title.</value>
+    public string title { get; set; }
+    
+
+
+    public NavigationCategory() {
+    }
+  }
+}
+
---
+++ ION/ION.Droid/src/Widgets/Adapters/Navigation/NavigationItem.cs
@@ -0,0 +1,51 @@
+﻿namespace ION.Droid.Widgets.Adapters.Navigation {
+
+  using System;
+
+  using Android.Content;
+  using Android.Graphics.Drawables;
+
+  /// <summary>
+  /// A simple container for items that will navigation the user to various parts
+  /// of the app upon their action being fired.
+  /// </summary>
+  public class NavigationItem {
+    /// <summary>
+    /// The value that is used to identify this explicity item within a navigation
+    /// list.
+    /// </summary>
+    /// <value>The identifier.</value>
+    public int id { get; private set; }
+    /// <summary>
+    /// The action that will be fired when the item is clicked.
+    /// </summary>
+    /// <value>The action.</value>
+    public Action action { get; set; }
+    /// <summary>
+    /// The icon for the navigation item.
+    /// </summary>
+    /// <value>The icon.</value>
+    public Drawable icon { get; set; }
+    /// <summary>
+    /// The title of the navigation item.
+    /// </summary>
+    /// <value>The title.</value>
+    public string title { get; set; }
+
+    public NavigationItem(int id) {
+      this.id = id;
+    }
+
+    public NavigationItem(Context context, int id, int icon, int title, Action action) 
+      : this(id, context.GetDrawable(icon), title, action) {
+    }
+
+    public NavigationItem(int id, Drawable icon, string title, Action action) {
+      this.id = id;
+      this.icon = icon;
+      this.title = title;
+      this.action = action;
+    }
+  }
+}
+
