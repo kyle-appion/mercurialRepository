49c0f4df87c4c5f7831dccd5b4c84d178a117613
Andrew Hodder <ahodder@appioninc.com>
2016-01-11 09:43:54 -0700

    Merged

--- ION/ION.IOS/src/ViewController/Analyzer/holdTableCell.cs
+++ ION/ION.IOS/src/ViewController/Analyzer/holdTableCell.cs
@@ -5,16 +5,23 @@ using CoreGraphics;
 
 namespace ION.IOS.ViewController.Analyzer {
   public partial class holdTableCell : UITableViewCell {
-    UILabel cellHeader = new UILabel(new CGRect(0,0,149, 30));
-    UILabel cellReading = new UILabel(new CGRect(30,30,119,30));
+    UILabel cellHeader;
+    UILabel cellReading;
     UILabel holdLabel = new UILabel();
-    UIButton cellButton = new UIButton(new CGRect(0,30,30,30));
+    UIButton cellButton;
   
     public holdTableCell(IntPtr handle) {
       
     }
 
-    public void makeEvents(lowHighSensor lhSensor){
+    public void makeEvents(lowHighSensor lhSensor, CGRect tableRect){
+      //UILabel cellHeader = new UILabel(new CGRect(0,0,149, 30));
+      cellHeader = new UILabel(new CGRect(0,0, 1.006 * tableRect.Width, .5 * lhSensor.cellHeight));
+      //UILabel cellReading = new UILabel(new CGRect(30,30,121,30));
+      cellReading = new UILabel(new CGRect(.2 * tableRect.Width, .5 * lhSensor.cellHeight, .8 * tableRect.Width, .5 * lhSensor.cellHeight));
+      //UIButton cellButton = new UIButton(new CGRect(0,30,30,30));
+      cellButton = new UIButton(new CGRect(0, .5 * lhSensor.cellHeight, .2 * tableRect.Width, .5 * lhSensor.cellHeight));
+
       cellHeader.Text = "HOLD";
       cellHeader.TextColor = UIColor.White;
       cellHeader.BackgroundColor = UIColor.Black;
@@ -22,7 +29,9 @@ namespace ION.IOS.ViewController.Analyzer {
       cellHeader.TextAlignment = UITextAlignment.Center;
       cellHeader.AdjustsFontSizeToFitWidth = true;
 
-      cellReading.Text = "";
+      if (cellReading.Text == "") {
+        cellReading.Text = "";
+      }
       cellReading.TextAlignment = UITextAlignment.Right;
       cellReading.Font = UIFont.FromName("Helvetica", 14f);
       cellReading.AdjustsFontSizeToFitWidth = true;
@@ -36,7 +45,11 @@ namespace ION.IOS.ViewController.Analyzer {
       holdLabel = lhSensor.holdReading;
 
       cellButton.TouchUpInside += delegate {
-        cellReading.Text = holdLabel.Text;
+        if(lhSensor.isManual){
+          cellReading.Text = lhSensor.LabelMiddle.Text + " " + lhSensor.LabelBottom.Text;
+        } else {
+          cellReading.Text = holdLabel.Text;
+        }
       };
 
       this.AddSubview(cellHeader);
