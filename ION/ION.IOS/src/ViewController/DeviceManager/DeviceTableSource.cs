51c8249277b4ca17c7ddf42c7cc3268749ac981a
Andrew Hodder <ahodder@appioninc.com>
2016-01-14 14:01:22 -0700

    Started device manager fix

---
+++ ION/ION.IOS/src/ViewController/DeviceManager/DeviceTableSource.cs
@@ -0,0 +1,13 @@
+﻿namespace ION.IOS.ViewController.DeviceManager {
+  
+  using System;
+
+  using Foundation;
+  using UIKit;
+
+  public class DeviceTableSource : UITableViewSource {
+    public DeviceTableSource() {
+    }
+  }
+}
+
