876af770865e95827d19ba2045f7915c9b723511
Kyle Johnson <kyle@appioninc.com>
2016-01-15 15:35:49 -0700

    Analyzer low high area shsc cell unit updated

---
+++ ION/ION.IOS/src/ViewController/Analyzer/ManualDeviceSensor.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using ION.Core.Sensors;
+
+namespace ION.IOS {
+  public class ManualDeviceSensor : Sensor {
+    
+    public ManualDeviceSensor(ESensorType sensorType,bool relative = true): base(sensorType, relative, true) {
+      
+    }
+
+  }
+}
+
