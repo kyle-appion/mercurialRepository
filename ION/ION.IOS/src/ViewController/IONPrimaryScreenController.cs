a381dfb4f3f5ed90e2a009df6e37234ac298de92
Andrew Hodder <ahodder@appioninc.com>
2016-06-22 12:01:57 -0600

    Fixed a couple of potential release issues.

--- ION/ION.IOS/src/ViewController/IONPrimaryScreenController.cs
+++ ION/ION.IOS/src/ViewController/IONPrimaryScreenController.cs
@@ -254,10 +254,8 @@ namespace ION.IOS.ViewController {
         new UINavigationController(InflateViewController<AnalyzerViewController>(BaseIONViewController.VC_ANALYZER)),
         new UINavigationController(InflateViewController<PTChartViewController>(BaseIONViewController.VC_PT_CHART)),
         new UINavigationController(InflateViewController<SuperheatSubcoolViewController>(BaseIONViewController.VC_SUPERHEAT_SUBCOOL)),
-#if DEBUG
         new UINavigationController(InflateViewController<JobViewController>(BaseIONViewController.VC_JOB_MANAGER)),
         new UINavigationController(InflateViewController<LoggingViewController>(BaseIONViewController.VC_LOGGING)),
-#endif
         null, // Screenshot Navigation
         null, // Settings navigation
         null, // Help Navigation
