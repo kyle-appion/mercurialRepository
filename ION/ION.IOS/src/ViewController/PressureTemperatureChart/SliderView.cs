cb21ac92e6f4b6b7f04caa298083cad961944ee0
Kyle Johnson <kyle@appioninc.com>
2016-08-24 10:14:06 -0600

    PTslider ptchart update fixed when using a gauge

--- ION/ION.IOS/src/ViewController/PressureTemperatureChart/SliderView.cs
+++ ION/ION.IOS/src/ViewController/PressureTemperatureChart/SliderView.cs
@@ -64,29 +64,36 @@ namespace ION.IOS.ViewController.PressureTemperatureChart {
     
     public async void setOffsetFromSensorMeasurement(Sensor sensor){
     	await Task.Delay(TimeSpan.FromMilliseconds(5)); 
-		if(sensor is GaugeDeviceSensor){
-			if(sensor.type == ESensorType.Pressure){
-				if(sensor.measurement.amount >= ptView.minPressure && sensor.measurement.amount <= ptView.maxPressure.amount){
-					if(ptView.lookup != sensor.unit){
-						ptView.resetData(Chart,sensor.unit,ptView.tempLookup);
-						await Task.Delay(TimeSpan.FromMilliseconds(2));
+			if(sensor is GaugeDeviceSensor){
+				Console.WriteLine("SliderView.cs - Unit " + ptView.lookup +"/" + sensor.unit);
+				if(sensor.type == ESensorType.Pressure){
+					if(sensor.measurement.amount >= ptView.minPressure && sensor.measurement.amount <= ptView.maxPressure.amount){
+						if(ptView.lookup != sensor.unit){
+							Console.WriteLine("SliderView.cs - Unit mismatch");
+							ptView.resetData(Chart,sensor.unit,ptView.tempLookup);
+							await Task.Delay(TimeSpan.FromMilliseconds(2));
+						}
+						Console.WriteLine("SliderView.cs - Pressure amount " + sensor.measurement.amount + " Pressure unit " + sensor.unit + " Temperature Unit " + ptView.tempLookup);
+						Console.WriteLine("SliderView.cs - Chart Fluid " + Chart.fluid);						
+						var convertedTemperature = Chart.GetTemperature(new Scalar(sensor.unit,sensor.measurement.amount),sensor.isRelative).ConvertTo(ptView.tempLookup);
+						var tempOffset = (convertedTemperature.amount - ptView.minTemperature.amount) * ptView.tempTicks;
+						Console.WriteLine("SliderView.cs - Temp " + convertedTemperature);
+						Console.WriteLine("SliderView.cs - Min Temp " + ptView.minTemperature);
+						Console.WriteLine("SliderView.cs - Offset " + tempOffset + " tempticks " + ptView.tempTicks);
+						ptScroller.SetContentOffset(new CGPoint(tempOffset,0),true);
 					}
-					var convertedTemperature = Chart.GetTemperature(new Scalar(sensor.unit,sensor.measurement.amount),sensor.isRelative).ConvertTo(ptView.tempLookup);
-					var tempOffset = (convertedTemperature.amount - ptView.minTemperature.amount) * ptView.tempTicks;
-					ptScroller.SetContentOffset(new CGPoint(tempOffset,0),true);
-				}
-			 } else if (sensor.type == ESensorType.Temperature){
-				if(sensor.measurement.amount >= ptView.minTemperature.amount && sensor.measurement.amount <= ptView.maxTemperature){
-					if(ptView.tempLookup != sensor.unit){
-						ptView.resetData(Chart,ptView.lookup,sensor.unit);
-						await Task.Delay(TimeSpan.FromMilliseconds(2));
+				 } else if (sensor.type == ESensorType.Temperature){
+					if(sensor.measurement.amount >= ptView.minTemperature.amount && sensor.measurement.amount <= ptView.maxTemperature){
+						if(ptView.tempLookup != sensor.unit){
+							ptView.resetData(Chart,ptView.lookup,sensor.unit);
+							await Task.Delay(TimeSpan.FromMilliseconds(2));
+						}
+						var tempOffset = (sensor.measurement.amount - ptView.minTemperature.amount) * ptView.tempTicks + ptView.startGap;
+						ptScroller.SetContentOffset(new CGPoint(tempOffset,0),true);
 					}
-					var tempOffset = (sensor.measurement.amount - ptView.minTemperature.amount) * ptView.tempTicks + ptView.startGap;
-					ptScroller.SetContentOffset(new CGPoint(tempOffset,0),true);
-				}
+				 }
 			 }
-		 }
-	 }
+	 	}
 
     public void RedrawMeasurements(PTChart ptChart){
       ptView.SetNeedsDisplay();
