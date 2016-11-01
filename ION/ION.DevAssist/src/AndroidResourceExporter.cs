ec7937dc37ad4bd3681422b92dd3f012cc8cdef1
Andrew Hodder <ahodder@appioninc.com>
2016-11-01 16:02:13 -0600

    Got a working template for ION string localization for dev assist

--- ION/ION.DevAssist/src/AndroidResourceExporter.cs
+++ ION/ION.DevAssist/src/AndroidResourceExporter.cs
@@ -1,16 +1,69 @@
 ﻿namespace ION.DevAssist {
 
 	using System;
+	using System.Collections.Generic;
 	using System.IO;
 	using System.Xml;
 	using System.Xml.Linq;
 
 	public class AndroidResourceExporter {
 
+		private const string EL_STRING = "string";
+		private const string EL_NAME = "name";
+
 		public static void ExportAndroidStrings(FileInfo[] files) {
+
+			var strings = new Dictionary<string, string>();
+
 			foreach (var fi in files) {
-				ExportFile(fi);
+				ReadStringFile(fi, strings);
 			}
+
+			ExportStringsToCsv(strings, File.Open("TESTO_android.csv", FileMode.Create));
+		}
+
+		/// <summary>
+		/// Reads the android file into the strings dictionary.
+		/// </summary>
+		/// <param name="fi">Fi.</param>
+		/// <param name="strings">Strings.</param>
+		private static void ReadStringFile(FileInfo fi, Dictionary<string, string> strings) {
+			var stream = File.Open(fi.FullName, FileMode.Open);
+			var root = XElement.Load(XmlReader.Create(stream));
+
+			foreach (var element in root.Elements()) {
+				if (EL_STRING.Equals(element.Name.LocalName)) {
+					var tu = ParseString(element);
+					strings[tu.Item1] = tu.Item2;
+				}
+			}
+		}
+
+		/// <summary>
+		/// Parses a single string element.
+		/// </summary>
+		/// <returns>The string.</returns>
+		/// <param name="e">E.</param>
+		private static Tuple<string, string> ParseString(XElement e) {
+			return new Tuple<string, string>(e.Attribute(EL_NAME).Value, e.Value);
+		}
+
+		/// <summary>
+		/// Exports the strings.
+		/// </summary>
+		/// <param name="strings">Strings.</param>
+		private static void ExportStringsToCsv(Dictionary<string, string> strings, Stream outStream) {
+			var csv = new Csv();
+
+			foreach (var key in strings.Keys) {
+				var row = new Csv.Row();
+				csv.AddRow(row);
+
+				row.AddCol(key);
+				row.AddCol(strings[key]);
+			}
+
+			csv.Export(outStream);
 		}
 	}
 }
