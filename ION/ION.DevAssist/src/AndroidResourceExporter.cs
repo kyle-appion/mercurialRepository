baaf77cd39378cc571d7b2e8fa3766bd702b228e
Andrew Hodder <ahodder@appioninc.com>
2016-11-01 14:36:21 -0600

    Started working on localization utility

---
+++ ION/ION.DevAssist/src/AndroidResourceExporter.cs
@@ -0,0 +1,16 @@
+﻿namespace ION.DevAssist {
+
+	using System;
+	using System.IO;
+	using System.Xml;
+	using System.Xml.Linq;
+
+	public class AndroidResourceExporter {
+
+		public static void ExportAndroidStrings(FileInfo[] files) {
+			foreach (var fi in files) {
+				ExportFile(fi);
+			}
+		}
+	}
+}
