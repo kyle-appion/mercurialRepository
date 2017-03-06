38d04b3c8d8e183b576f56e518259b520b8e8114
Andrew Hodder <ahodder@appioninc.com>
2017-03-06 09:06:26 -0700

    Started the remote analzyer portal work

---
+++ ION/ION.CoreExtensions/src/Net/Portal/Remote/RemoteAnalyzer.cs
@@ -0,0 +1,21 @@
+﻿namespace ION.CoreExtensions.Net.Portal.Remote {
+
+	using System;
+
+	using Newtonsoft.Json;
+
+	public class RemoteAnalyzer {
+		[JsonProperty("sn")]
+		public string serialNumber;
+		[JsonProperty("si")]
+		public string index;
+		[JsonProperty("sl")]
+		public string sensorMountIndex;
+		[JsonProperty("sv")]
+		public string value;
+		[JsonProperty("su")]
+		public string unit;
+		[JsonProperty("sa")]
+		public string stuff;
+	}
+}
