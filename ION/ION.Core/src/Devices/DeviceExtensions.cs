a6627934f2affe3becd890b3919445dfe0d2a5f4
Andrew Hodder <ahodder@appioninc.com>
2017-08-01 09:28:06 -0600

    Fixed the majority of the padding issue for subviews

--- ION/ION.Core/src/Devices/DeviceExtensions.cs
+++ ION/ION.Core/src/Devices/DeviceExtensions.cs
@@ -1,14 +1,26 @@
-﻿using System.Linq;
+using System.Collections.Generic;
+using System.Linq;
+
+using ION.Core.App;
+using ION.Core.Connections;
 
 namespace ION.Core.Devices {
-  
-  using System.Collections.Generic;
-  public static class DeviceExtensions {
 
+  public static class DeviceExtensions {
     public static void GeneralSort(this IEnumerable<GaugeDeviceSensor> enumerable) {
       enumerable.OrderBy(x => x.device.serialNumber.deviceModel)
         .ThenBy(x => x.device.serialNumber)
         .ThenBy(x => x.index);
     }
+
+#if DEBUG
+    public static IDevice CreateDebugDevice(IION ion, EDeviceModel deviceModel) {
+      var sn = deviceModel.GenerateSerialNumber();
+
+      var factory = ion.deviceManager.deviceFactory;
+      var defn = factory.GetDeviceDefinition(sn);
+      return defn.CreateDevice(sn, new DebugConnection(sn.ToString(), "DebugAddress_" + sn.batchId), null);
+    }
+#endif
   }
 }
